c3b78c347759dbe315f05bee5780be071e0854fe
Jesewe <81848878+Jesewe@users.noreply.github.com>
2022-03-15 20:57:41 +0600

    Update wallhack.py

--- wallhack.py
+++ wallhack.py
@@ -6,8 +6,8 @@ client = pymem.process.module_from_name(pm.process_handle,
                                         'client.dll')
 
 clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x83\xE0\x0F\x80\xBF',
-                                         clientModule).start() + 9
+address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
+                                         clientModule).start() + 2
 
-pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
+pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
 pm.close_process()
